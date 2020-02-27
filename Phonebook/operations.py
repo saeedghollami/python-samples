e07b15fa1bf7e6c711e1b875a824053fff0c06a2
Saeed Gholami <saeed.ghollami@gmail.com>
2020-02-28 03:01:24 +0330

    Update a contact by id

--- Phonebook/operations.py
+++ Phonebook/operations.py
@@ -53,67 +53,20 @@ def read_contacts(conn):
     return cur.fetchall()
 
 
-# # save contacts in a pickle file
-# def save_contacts(filename='contacts.pickle'):
-#     with open(filename, 'wb') as contact_db:
-#         # write contacts using pickle 
-#         pickle.dump(contacts, contact_db)
+def update_contact(conn, new_contact):
+    sql = """
+        UPDATE contact
+        SET first = ?,
+            last = ?,
+            phone = ?
+        WHERE id = ?
+    """
+    cur = conn.cursor()
+    cur.execute(sql, new_contact)
+    return True
 
 
 
-# # read contacts from pickle file
-# def load_contacts(filename='contacts.pickle'):
-#     global contacts
-#     with open(filename, 'rb') as contact_db:
-#         contacts = pickle.load(contact_db)
-
-
-
-
-
-
-# # get user infromation and create a contact with them
-# def create_contact(info):
-#     first, last, phone = info
-#     contact = {'id': len(contacts)+1,  'first_name': first,
-#                'last_name': last, 'phone_number': phone}
-#     contacts.append(contact)
-#     return True
-
-
-# # get id of the contact and update it with new info
-# def update_contact(idx):
-#     contact = find_contact(idx)
-#     if contact:
-#         # get new info for the contact
-#         first, last, phone = get_info()
-#         # make a new contact
-#         contact['first_name'] = first
-#         contact['last_name'] = last
-#         contact['phone_number'] = phone
-#         return True
-#     else:
-#         return False
-
-
-# # get id of contact if the contact exist, delete
-# def delete_contact(idx):
-#     contact = find_contact(idx)
-#     if contact:
-#         index = contacts.index(contact)
-#         del contacts[index]
-#         return True
-#     else:
-#         return False
-
-
-# # get id of the contact and return contact if exist, else return False
-# def find_contact(idx):
-#     for contact in contacts:
-#         cid, *_ = contact.values()
-#         if int(cid) == idx:
-#             return contact
-#     return False
 
 
 if __name__ == "__main__":
@@ -127,5 +80,14 @@ if __name__ == "__main__":
     # result = create_contact(conn, c)
     # print(result)
 
+    print("before update")
+    contacts = read_contacts(conn)
+    print(contacts)
+
+    new_c = ('Jack', 'Doe', '234', 2)
+    r = update_contact(conn, new_c)
+    print(r)
+
+    print("After update")
     contacts = read_contacts(conn)
     print(contacts)
