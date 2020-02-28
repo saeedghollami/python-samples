c209418a8e469568225abd1d14e1793792b4a64d
Saeed Gholami <saeed.ghollami@gmail.com>
2020-02-28 05:08:56 +0330

    Read all contacts and return them as list

--- Phonebook/ponyorm_demo.py
+++ Phonebook/ponyorm_demo.py
@@ -2,14 +2,15 @@ from pony.orm import *
 
 
 
-
-
 db = Database()
+
+
 
 class Contact(db.Entity):
     first = Required(str)
     last = Required(str)
     phone = Required(str)
+
 
 db.bind(provider='sqlite', filename='phonebook_pony.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
@@ -21,5 +22,30 @@ def create_contact(first, last, phone):
     c = Contact(first=first, last=last, phone=phone)
 
 
+@db_session
+def read_contacts():
+	return Select(c for c in Contact)[:]
+
+
+@db_session
+def find_contact():
+	pass
+
+
+@db_session
+def update_contact():
+	pass
+
+
+@db_session
+def delete_contact():
+	pass
+
+
+
 if __name__ == "__main__":
-	create_contact('John', 'Doe', '123')
+	# create_contact('John', 'Doe', '123')
+
+	contacts = read_contacts()
+	print(dir(contacts))
+	print(contacts)
